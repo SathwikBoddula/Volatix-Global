8c682eb7b746cf181e5ffb1d110f9451fe9a498e
Sathwik Boddula <sathwikboddula05@gmail.com>
2026-09-10 22:05:58 +0530

    fix(style): load Tailwind typography plugin in ESM

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -98,7 +100,7 @@ export default {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [typography],
   safelist: [
     'text-positive',
     'text-negative',
